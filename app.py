0c04e3ac7554bbed5b95da53b6574295f55b0df0
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2024-06-18 22:23:23 +1200

    created connetcion with future database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pymysql
 
 app = Flask(__name__)
+
+def createConnection():
+    return pymysql.connect(
+        host = "localhost",
+        port = 3306,
+        user = "SaharaAdmin",
+        password="ezravsprawn",
+        db = "",
+        cursorclass = pymysql.cursors.DictCursor
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True)
