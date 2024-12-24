4d6b0dddb9bd97da53f0a0cc250a9f481be05b54
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2024-12-24 16:39:13 +1300

    managed to fix admin uploading image, still dont know if it works cuz im having issues with the local server

--- app.py
+++ app.py
@@ -5,36 +5,49 @@ from io import BytesIO
 from PIL import Image
 import io
 
+import random
+
 engine = create_engine("mysql+pymysql://SaharaAdmin:abcdefg123@localhost:3306/productdb")
 
 app = Flask(__name__)
 
 @app.route("/")
 def Home():
-    return render_template("home.html")
+    randomProducts = []
+    try:
+        with engine.connect() as connection:
+            query = text("SELECT * FROM allproducts")
+            result = connection.execute(query)
+            randomIn = [random.randint(0,len(list(result))+1)]
+            for i in randomIn:
+                randomProducts.append(dict(list(result)[i]))
+                randomIn.remove(i)
+    except:
+        pass
+    return render_template("home.html",randomProducts=randomProducts)
 
 @app.route("/admin")
 def Admin():
     return render_template("admin.html")
 
-@app.route("/adminUpdate",methods=["POST"])
+@app.route("/adminUpdate", methods=["POST"])
 def adminUpdate():
-    id = request.form["productID"]
     name = request.form["productName"]
     price = request.form["productPrice"]
 
-    image = Image.open(request.form["productImage"])
-    image = image.resize((500,500),Image.LANCZOS)
+    image = Image.open(request.files["productImage"])
 
     imageByte = io.BytesIO()
     image.save(imageByte, format=image.format)
     imageByte = imageByte.getvalue()
 
     with engine.connect() as connection:
-        query = text("INSERT INTO allproducts (id, name, price, image) VALUES (:id, :name, :price, :image)")
-        connection.execute(query, {"id": id, "name": name, "price": price, "image": imageByte})
-    
-    return True
+        query = text(
+            "INSERT INTO allproducts (productName, productPrice, productImage) VALUES (:name, :price, :image)"
+        )
+        connection.execute(query, {"name": name, "price": price, "image": imageByte})
+        
+    return render_template("admin.html")
 
 def getImageMimeType(imageBytes) -> str:
     image = Image.open(BytesIO(imageBytes))
