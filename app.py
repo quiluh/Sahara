d131cca0259eaa9e2e917bd4a3450ca248639b77
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-01-01 19:48:07 +1300

    started working on search bar functionality + visuals

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 
 from sqlalchemy import text, create_engine
 from io import BytesIO
---
+++ static/scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById("searchForm").addEventListener("keydown",function(event) {
+    if (event.key == "enter") {
+        
+    }
+})
