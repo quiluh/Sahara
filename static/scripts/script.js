a31951f7849af3896ec11ba193cb05d7eb3ebd6e
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-01-03 23:45:08 +1300

    javascript now sends the user's search input to the python, havent coded what happens when it receives the result back from the python

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,6 +1,17 @@
 const searchForm = document.getElementById("searchForm");
-searchForm.addEventListener("keydown",async function(event) {
+searchForm.addEventListener("keydown",function(event) {
     if (event.key == "enter") {
-        
+        $.ajax({
+            url: "/processSearch",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({"result":searchForm.value}),
+            success: function(response) {
+                // successful response goes here
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
     }
 })
