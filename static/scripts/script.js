b9c956e08862f8a88cb09f30fe3a29298db25e8f
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-01-04 23:32:55 +1300

    created elements in js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,5 @@
 const searchForm = document.getElementById("searchForm");
+const searchResultDiv = document.getElementById("searchResultDiv");
 searchForm.addEventListener("keydown",function(event) {
     if (event.key == "enter") {
         $.ajax({
@@ -7,7 +8,25 @@ searchForm.addEventListener("keydown",function(event) {
             contentType: "application/json",
             data: JSON.stringify({"result":searchForm.value}),
             success: function(response) {
-                // successful response goes here
+                let searchList = response.result;
+
+                let card = document.createElement("div");
+                card.className = "card cardDiv m-auto w-50 mt-1";
+
+                let clickablility = document.createElement("a");
+                clickablility.href = "";
+                clickablility.className = "text-decoration-none";
+
+                let image = document.createElement("img");
+                image.src = "";
+                image.className = "card-img-top w-50 m-auto pt-1";
+
+                let cardBody = document.createElement("div");
+                cardBody.className = "card-body text-center";
+
+                let cardText = document.createElement("p");
+                cardText.innerHTML = "";
+                cardText.className = "card-text searchCardText";
             },
             error: function(error) {
                 console.log(error);
