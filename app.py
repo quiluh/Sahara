3d892f5da375b399fef451468d2e335bf7d819e7
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-01-08 18:46:09 +1300

    removed unused package

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request, jsonify
 
 from sqlalchemy import text, create_engine, inspect
 
