d5114ac8cc8b322c530fb2bb0a7e30fa36a8908c
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-01-13 12:07:54 +1300

    changed what im sending to js, made more foolproof

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def processSearch():
 def handleAddToCart():
     data = request.get_json()
     if UserData.addToCart(data["productID"]):
-        return jsonify(result=True)
+        return jsonify(result=data["productID"])
 
 if __name__ == "__main__":
     app.run()
